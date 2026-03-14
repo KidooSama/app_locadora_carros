2ddc93c0a436831ab6b6620324c7e844b7e67abc
Talles Braga <tallesbraga63@gmail.com>
2026-03-14 19:44:32 -0300

    feat: implement ClienteController with repository pattern, add StoreClienteRequest and UpdateClienteRequest validation, and create ClienteRepository

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -5,17 +5,25 @@ namespace App\Http\Controllers;
 use App\Models\Cliente;
 use App\Http\Requests\StoreClienteRequest;
 use App\Http\Requests\UpdateClienteRequest;
+use App\repositories\ClienteRepository;
+use Illuminate\Http\Request;
 
 class ClienteController extends Controller
 {
+    protected $cliente;
+
+    public function __construct(Cliente $cliente){
+        $this->cliente = $cliente;
+    }
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+      $clienteRepository = new ClienteRepository($this->cliente);
+      return  response()->json(['data'=>$clienteRepository->getResultado()], 200);
     }
 
 
@@ -27,7 +35,10 @@ class ClienteController extends Controller
      */
     public function store(StoreClienteRequest $request)
     {
-        //
+        $cliente = $this->cliente->create([
+            'nome'=> $request->nome
+        ]);
+        return response()->json(['data'=> $cliente,'message' => 'Criado com sucesso'], 201);
     }
 
     /**
@@ -36,9 +47,13 @@ class ClienteController extends Controller
      * @param  \App\Models\Cliente  $cliente
      * @return \Illuminate\Http\Response
      */
-    public function show(Cliente $cliente)
+    public function show($id)
     {
-        //
+        $cliente = $this->cliente->find($id);
+        if ($cliente === null) {
+           return response()->json(['message'=>'Valor não encontrado'], 404);
+        }
+        return response()->json(['data'=>$cliente], 200);        
     }
 
     /**
@@ -48,9 +63,15 @@ class ClienteController extends Controller
      * @param  \App\Models\Cliente  $cliente
      * @return \Illuminate\Http\Response
      */
-    public function update(UpdateClienteRequest $request, Cliente $cliente)
+    public function update(UpdateClienteRequest $request, $id)
     {
-        //
+        $cliente = $this->cliente->find($id);
+        if($cliente === null) {
+            return response()->json(['erro' => 'Impossível realizar a atualização. O recurso solicitado não existe'], 404);
+        }
+        $cliente->fill($request->all());
+        $cliente->save();
+        return response()->json($cliente, 200);
     }
 
     /**
@@ -59,8 +80,14 @@ class ClienteController extends Controller
      * @param  \App\Models\Cliente  $cliente
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Cliente $cliente)
+    public function destroy($id)
     {
-        //
+        $cliente = $this->cliente->find($id);
+        if ($cliente === null) {
+            return response()->json(['message'=>'Valor Não Encontrado'], 404);
+        }   
+        $cliente->delete();
+        
+        return response()->json(['data'=>$cliente], 200);
     }
 }
--- app/Http/Requests/UpdateClienteRequest.php
+++ app/Http/Requests/UpdateClienteRequest.php
@@ -13,7 +13,7 @@ class UpdateClienteRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,7 @@ class UpdateClienteRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+           'nome' => 'sometimes|required|min:3|max:255|'
         ];
     }
 }
