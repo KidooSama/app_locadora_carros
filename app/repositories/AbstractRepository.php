5b5839359c6593fe44c688fce81002af872115a0
Talles Braga <tallesbraga63@gmail.com>
2026-03-13 21:45:34 -0300

    refactor: consolidate common repository methods into AbstractRepository for better code reuse

--- app/repositories/AbstractRepository.php
+++ app/repositories/AbstractRepository.php
@@ -3,7 +3,29 @@
 namespace App\repositories;
 use Illuminate\Database\Eloquent\Model;
 
-class MarcaRepository{
+class AbstractRepository{
+    protected $model;
+    public function __construct(Model $model){
+        $this->model = $model;       
+    }
+    public function selectAtributosRegistros($atributos){
+        $this->model = $this->model->with($atributos);
+    }
+    public function filtro($filtros){
+        $filtros = explode(';',$filtros);
+        foreach ($filtros as $condicao) {          
+            $condicao = explode(':',$condicao);
+            $this->model = $this->model->where($condicao[0], $condicao[1], $condicao[2]);
+        }
+    }
+    public function selectAtributos($atributos){
+       $this->model = $this->model->select(explode(',',$atributos));
+    
+    }
+    public function getResultado(){
+        return $this->model->get();
+    }
+
 
 }
 
--- app/repositories/ModeloRepository.php
+++ app/repositories/ModeloRepository.php
@@ -1,32 +1,11 @@
 <?php
 
 namespace App\repositories;
-use Illuminate\Database\Eloquent\Model;
 
-class ModeloRepository{
-    protected $model;
-    public function __construct(Model $model){
-        $this->model = $model;       
-    }
-    public function selectAtributosRegistros($atributos){
-        $this->model = $this->model->with($atributos);
-    }
-    public function filtro($filtros){
-        $filtros = explode(';',$filtros);
-        foreach ($filtros as $condicao) {          
-            $condicao = explode(':',$condicao);
-            $this->model = $this->model->where($condicao[0], $condicao[1], $condicao[2]);
-        }
-    }
-    public function selectAtributos($atributos){
-       $this->model = $this->model->select(explode(',',$atributos));
-    
-    }
-    public function getResultado(){
-        return $this->model->get();
-    }
-
-
+class ModeloRepository extends AbstractRepository{
 
 }
+
+
+
 ?>
