0dfc653cda1c22e67802f7a4ce5fe723f9df87b6
Talles Braga <tallesbraga63@gmail.com>
2026-03-19 17:44:10 -0300

    feat: implement JWT authentication in AuthController, update routes for JWT middleware, and extend token lifespan

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,5 +6,29 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function login(Request $request){
+        
+        $cred = $request->all(['email','password']);
+        $token = auth('api')->attempt($cred);
+
+        if ($token) {
+            return response()->json(['token'=> $token]);    
+        }else{
+            return response()->json(['erro'=> 'Usuário Inválido'], 403);
+        }
+    }
+    public function logout(){
+        auth('api')->logout();
+        return response()->json(['mesage'=> 'Usuario foi deslogado']);
+    }
+    
+    public function refresh(){
+    /** @var \Tymon\JWTAuth\JWTGuard  */
+        $auth = auth('api');
+        $token = $auth->refresh();
+        return response()->json(['token'=> $token]);
+    }
+    public function me(){
+        return response()->json(['data'=>auth()->user()]);
+    }
 }
